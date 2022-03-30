187f2dac40f67fea7f2a40924dcd8e72aa97192e
Philip Loche <ploche@physik.fu-berlin.de>
2022-03-30 16:13:18 +0200

    Added SPC/E volume

---
+++ src/PySnips/constants.py
@@ -0,0 +1,6 @@
+# -*- Mode: python3; tab-width: 4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
+"""Physical and other constants not defined elsewhere."""
+
+# Volume of a SPC/E water molecule nm^3
+v_spce = 0.0304
